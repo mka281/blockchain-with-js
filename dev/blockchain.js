805821d978252d9c25f40eb0e1a7d6e4f1b9d185
mka <mka281@icloud.com>
2018-08-20 23:35:55 -0700

    Add create new block method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,4 +1,20 @@
 function Blockchain() {
   this.chain = [];
-  this.newTransaction = [];
+  this.newTransactions = [];
 }
+
+Blockchain.prototype.createBlock = function(nonce, previousBlockHash, hash) {
+  const newBlock = {
+    index: this.chain.length + 1,
+    timestamp: Date.now(),
+    transactions: this.newTransactions,
+    nonce: nonce,
+    hash: hash,
+    previousBlockHash: previousBlockHash
+  };
+
+  this.newTransactions = [];
+  this.chain.push(newBlock);
+
+  return newBlock;
+};
