e4dd69a0d4314083320eac66ae9bc35f76dddd83
mka <mka281@icloud.com>
2018-08-23 23:36:39 -0700

    Create genesis block

--- dev/test.js
+++ dev/test.js
@@ -1,24 +1,4 @@
 const Blockchain = require("./blockchain");
 const bitcoin = new Blockchain();
 
-const previousBlockHash = "ASD67S8F768GFSD68";
-const currentBlockData = [
-  {
-    amount: "234",
-    sender: "DFSGSDFGDF",
-    recipient: "DFGSDG5467"
-  },
-  {
-    amount: "43",
-    sender: "SDFASD6FS879",
-    recipient: "DSFGSDFG4356346"
-  },
-  {
-    amount: "777",
-    sender: "SDFGSDF8979SDFG",
-    recipient: "FSDGSDFG879879DFSG"
-  }
-];
-const nonce = 123;
-
-console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
+console.log(bitcoin);
