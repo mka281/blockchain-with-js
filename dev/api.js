5d34d3c0b60c82efb0143c8464c3fc1474fceee1
mka <mka281@icloud.com>
2018-08-24 12:15:57 -0700

    Build GET /mine endpoint

--- dev/api.js
+++ dev/api.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const Blockchain = require("./blockchain");
+const uuid = require("uuid/v1");
+
+const nodeAddress = uuid()
+  .split("-")
+  .join("");
 
 const bitcoin = new Blockchain();
 
@@ -18,6 +23,28 @@ app.post("/transaction", (req, res) => {
   res.json({ note: `Transaction will be added in block ${blockIndex}` });
 });
 
-app.get("/mine", (req, res) => {});
+app.get("/mine", (req, res) => {
+  const lastBlock = bitcoin.getLastBlock();
+  const previousBlockHash = lastBlock["hash"];
+  const currentBlockData = {
+    transactions: bitcoin.pendingTransactions,
+    index: lastBlock["index"] + 1
+  };
+  const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+  const blockHash = bitcoin.hashBlock();
+
+  bitcoin.createNewTransaction(12, "00", nodeAddress);
+
+  const newBlock = bitcoin.createNewBlock(
+    nonce,
+    previousBlockHash,
+    currentBlockData
+  );
+
+  res.json({
+    note: "New block mined successfully",
+    block: newBlock
+  });
+});
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
