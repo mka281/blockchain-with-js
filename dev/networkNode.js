8a2a1b992810a227233658b7fd79de1068ddf77a
mka <mka281@icloud.com>
2018-08-25 23:36:00 -0700

    Create different nodes

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const Blockchain = require("./blockchain");
 const uuid = require("uuid/v1");
+const port = process.argv[2];
 
 const nodeAddress = uuid()
   .split("-")
@@ -47,4 +48,4 @@ app.get("/mine", (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log("Listening on port 3000!"));
+app.listen(port, () => console.log(`Listening on port ${port}!`));
